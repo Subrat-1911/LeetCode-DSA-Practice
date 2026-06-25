105abc98df113c532a2d17c54b1588c9ca7500a6
Subrat Sahoo <subratsahoo28032006@gmail.com>
2026-06-25 09:55:05 +0530

    Auto-sync: Code for 142. Linked List Cycle II

---
+++ 142.linked-list-cycle-ii/142.linked-list-cycle-ii.cpp
@@ -0,0 +1,20 @@
+        }
+        if(flag == false) return NULL;
+    
+
+            }
+                break;
+            if(slow == fast){
+                flag = true;
+            slow = slow->next;
+            fast = fast->next->next;
+        bool flag = false; // no cicle
+        while(fast != NULL && fast->next != NULL){
+        ListNode* slow = head;
+        ListNode* fast = head;
+    ListNode *detectCycle(ListNode *head) {
+class Solution {
+public:
+ */
+ * };
+
