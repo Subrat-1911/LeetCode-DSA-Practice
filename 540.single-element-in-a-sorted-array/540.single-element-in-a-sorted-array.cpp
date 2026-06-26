bfc66c991df8160716ad91c5635b6acfcbbc69ea
Subrat Sahoo <subratsahoo28032006@gmail.com>
2026-06-26 20:00:21 +0530

    Auto-sync: Code for 540. Single Element in a Sorted Array

--- 540.single-element-in-a-sorted-array/540.single-element-in-a-sorted-array.cpp
+++ 540.single-element-in-a-sorted-array/540.single-element-in-a-sorted-array.cpp
@@ -1,20 +1,20 @@
-                //right side
-                lo=mid+1;
+class Solution {
+public:
+    int singleNonDuplicate(vector<int>& nums) {
+        int n = nums.size();
+        if(n==1) return nums[0];
+        int lo = 0;
+        int hi = n-1;
+        while(lo<=hi){
+            int mid = lo+(hi-lo)/2;
+            if(mid%2==0){
+                 if(nums[mid]==nums[mid+1]){
+                    //search rigt
+                    lo=mid+1;
+                }
+                else {
+                    hi=mid-1;
+                }
             }
             else {
-            if(nums[mid] > nums[hi]){
-
-            int mid = lo+(hi-lo)/2;
-            // if(nums[mid]<min){
-            //     min=nums[mid];
-            // }
-        //int min=INT_MAX;
-        while(lo<hi){
-        int lo =0;
-        int hi = n-1;
-        int n = nums.size();
-
-    int findMin(vector<int>& nums) {
-public:
-class Solution {
 
