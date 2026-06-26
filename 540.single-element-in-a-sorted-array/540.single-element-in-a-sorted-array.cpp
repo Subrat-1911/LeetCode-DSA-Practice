a1032291090fd2cf7fbd33033a6486c714d54c04
Subrat Sahoo <subratsahoo28032006@gmail.com>
2026-06-26 20:00:07 +0530

    Auto-sync: Code for 540. Single Element in a Sorted Array

---
+++ 540.single-element-in-a-sorted-array/540.single-element-in-a-sorted-array.cpp
@@ -0,0 +1,20 @@
+                //right side
+                lo=mid+1;
+            }
+            else {
+            if(nums[mid] > nums[hi]){
+
+            int mid = lo+(hi-lo)/2;
+            // if(nums[mid]<min){
+            //     min=nums[mid];
+            // }
+        //int min=INT_MAX;
+        while(lo<hi){
+        int lo =0;
+        int hi = n-1;
+        int n = nums.size();
+
+    int findMin(vector<int>& nums) {
+public:
+class Solution {
+
