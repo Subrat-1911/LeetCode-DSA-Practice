f8f5d07b8ee0d86bcc31fce14521a6e3c685a812
Subrat Sahoo <subratsahoo28032006@gmail.com>
2026-06-27 13:44:05 +0530

    Auto-sync: Code for 415. Add Strings

---
+++ 415.add-strings/415.add-strings.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    string addStrings(string num1, string num2) {
+        long long x=stoi(num1);
+        long long y=stoi(num2);
+
+        long long sum =x+y;
+        string s=to_string(sum);
+
+        return s;
+    }
+   
+};
+
