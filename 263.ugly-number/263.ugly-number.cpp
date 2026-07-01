02d1d0d7398a89dd1fd21952a4629a69748889ff
Subrat Sahoo <subratsahoo28032006@gmail.com>
2026-07-01 13:42:44 +0530

    Auto-sync: Code for 263. Ugly Number

--- 263.ugly-number/263.ugly-number.cpp
+++ 263.ugly-number/263.ugly-number.cpp
@@ -1,18 +1,19 @@
 class Solution {
 public:
     bool isUgly(int n) {
-        
-        // return false;
 
-        while(n>0){
-            n=n/2; if(n==1) return true;
+        while(n%2==0){
+            n=n/2;
+        }
+        if(n==1) return true;
+        if(n<=0) return false;
+        while(n%3==0){
+            n=n/3;
+        }
+        while(n%5==0){
+            n=n/5;
         }
     }
-        if(n==1) return true;
-
-            n=n/3; if(n==1) return true;
-            n=n/5; if(n==1) return true;
-        return false;
-        if(n<=0) return false;
+        else return false;
 };
 
