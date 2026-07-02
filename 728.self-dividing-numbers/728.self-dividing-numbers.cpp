c3f07cfabdc734b3d593f4566736822108003fa6
Subrat Sahoo <subratsahoo28032006@gmail.com>
2026-07-02 12:16:18 +0530

    Auto-sync: Code for 728. Self Dividing Numbers

---
+++ 728.self-dividing-numbers/728.self-dividing-numbers.cpp
@@ -0,0 +1,17 @@
+    vector<int> selfDividingNumbers(int left, int right) {
+    }
+        return flag;
+        }
+            }
+                flag=true;
+            if(x%z==0){
+            y=y/10;
+            int z= y%10;
+        while(y>0){
+        bool flag=false;
+        int y=x;
+    bool selfDive(int x){
+            else return flag=false;
+public:
+class Solution {
+
