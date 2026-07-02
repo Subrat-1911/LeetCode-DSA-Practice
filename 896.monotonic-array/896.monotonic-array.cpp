3cbd25279cb682cd7da0ed8f05bb0e0bab13d0ee
Subrat Sahoo <subratsahoo28032006@gmail.com>
2026-07-02 11:30:09 +0530

    Auto-sync: Code for 896. Monotonic Array

--- 896.monotonic-array/896.monotonic-array.cpp
+++ 896.monotonic-array/896.monotonic-array.cpp
@@ -1,17 +1,15 @@
-                }
-            }
         }
         else if(nums[0]>=nums[1]){
             for(int i=1;i<n-1;i++){
                 if(nums[i]>=nums[i+1]){
-                else flag=false;
+            }
+                else return flag=false;
                     flag=true;
-                if(nums[i]<=nums[i+1]){
-            for(int i=1;i<n-1;i++){
-        if(nums[0]<=nums[1]){
-        bool flag=true;
-        int n= nums.size();
-    bool isMonotonic(vector<int>& nums) {
-public:
-class Solution {
+                }
+                else return flag=false;
+            }
+        }
+        return flag;
+    }
+};
 
