98b4d11417f8b8bcb56d803f227081306998e158
Subrat Sahoo <subratsahoo28032006@gmail.com>
2026-07-02 10:23:18 +0530

    Auto-sync: Code for 283. Move Zeroes

--- 283.move-zeroes/283.move-zeroes.cpp
+++ 283.move-zeroes/283.move-zeroes.cpp
@@ -1,19 +1,17 @@
+        while(i<n && j<n){
+            if(nums[i]!=0){
+        }
+        int j=0;
+        int i=0;
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
         int n=nums.size();
-        int i=0;
-        while(i<n && j<n){
-            if(nums[i]==0 && nums[j]==0) j++;
-        }
-        int j=1;
-            else if(nums[i]==0){
                 swap(nums[i],nums[j]);
             }
-                i++;
-                j++;
-            else if(j==0) j++;
-            else i++;
     }
+                j++;
+            else i++;
+                i++;
 };
 
