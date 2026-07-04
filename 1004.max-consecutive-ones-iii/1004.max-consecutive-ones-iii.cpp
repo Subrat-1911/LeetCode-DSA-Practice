41f3c25b8d84a871608815c2d1dc747cdfb45d79
Subrat Sahoo <subratsahoo28032006@gmail.com>
2026-07-04 21:48:27 +0530

    Auto-sync: Code for 1004. Max Consecutive Ones III

---
+++ 1004.max-consecutive-ones-iii/1004.max-consecutive-ones-iii.cpp
@@ -0,0 +1,32 @@
+                for(int m=i;m<j;m++){
+                maxlen=max(maxlen,len);
+                len=j-i;
+            else{
+            }
+                j++;
+                flip++;
+            else if(flip<k){
+            if(nums[j]==1) j++;
+        while(j<n){
+        int maxlen=INT_MIN;
+                    if(nums[m]==0){
+                        i=m+1;
+                        j++;
+                        break;
+                    }
+                }
+
+
+            }
+            
+        }
+        return maxlen;
+    }
+        maxlen=max(maxlen,len);
+        int len=0;
+        int flip=0;
+        int j=0;
+        int i=0;
+        int n=nums.size();
+    int longestOnes(vector<int>& nums, int k) {
+
