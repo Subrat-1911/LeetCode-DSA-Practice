00d5d2911ae5c252b745bb5b1fadbf843ca5b2e2
Subrat Sahoo <subratsahoo28032006@gmail.com>
2026-07-04 09:28:14 +0530

    Auto-sync: Code for 2389. Longest Subsequence With Limited Sum

---
+++ 2389.longest-subsequence-with-limited-sum/2389.longest-subsequence-with-limited-sum.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<int> answerQueries(vector<int>& nums, vector<int>& queries) {
+        int n=nums.size();
+        int m=queries.size();
+        vector<int> ans(m);
+        sort(nums.begin(),nums.end());
+        for(int i=1;i<n;i++){
+            nums[i]+=nums[i-1];
+        }
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+        }
+                if(nums[j]<=queries[i]){
+            }
+                    ans[i]=j+1;
+                }
+    }
+        return ans;
+                el
+};
+
