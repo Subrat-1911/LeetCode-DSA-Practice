2370df13eb88eea4a3c84ef60c5cd96229783f59
Subrat Sahoo <subratsahoo28032006@gmail.com>
2026-07-04 09:02:07 +0530

    Auto-sync: Code for 1402. Reducing Dishes

---
+++ 1402.reducing-dishes/1402.reducing-dishes.cpp
@@ -0,0 +1,23 @@
+        for(int i=0;i<n;i++){
+            if(suf[i]>=0){
+        }
+                mindex=i;
+            }
+        int mindex=-1;
+        for(int i=mindex;i<n;i++){
+        int sat=1;
+            sum=sum+(satisfaction[i]*sat);
+        long sum=0;
+            sat++;
+        }
+            suf[i]=satisfaction[i]+suf[i+1];
+        for(int i=n-2;i>=0;i--){
+        suf[n-1]=satisfaction[n-1];
+        }
+        return sum;
+
+                break;
+        if(mindex==-1) return 0;
+    }
+};
+
