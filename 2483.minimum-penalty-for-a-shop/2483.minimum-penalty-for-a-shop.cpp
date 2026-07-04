a2fd55943761013b8b1ff53c1e27df57b83580f3
Subrat Sahoo <subratsahoo28032006@gmail.com>
2026-07-04 08:26:45 +0530

    Auto-sync: Code for 2483. Minimum Penalty for a Shop

--- 2483.minimum-penalty-for-a-shop/2483.minimum-penalty-for-a-shop.cpp
+++ 2483.minimum-penalty-for-a-shop/2483.minimum-penalty-for-a-shop.cpp
@@ -1,23 +1,10 @@
-            else if(noy>0){
-                suf[i]=noy;
-            }
-            else {
-                suf[i]=0;
-            }
-        }
-        for(int i=0;i<n+1;i++){
-            ans[i]=pre[i]+suf[i];
-        }
-        int min=INT_MAX;
-        int mindex=-1;
-        for(int i=0;i<n+1;i++){
             if(ans[i]<min){
                 min=ans[i];
-                mindex=i;
+                mindx=i;
             }
         }
-        return mindex;
-        
+        return mindx;
+
     }
 };
 
