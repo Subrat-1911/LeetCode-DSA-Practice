f2f687d477548400b703c11f1224c2ce72d6e601
Subrat Sahoo <subratsahoo28032006@gmail.com>
2026-07-04 20:57:23 +0530

    Auto-sync: Code for 209. Minimum Size Subarray Sum

---
+++ 209.minimum-size-subarray-sum/209.minimum-size-subarray-sum.cpp
@@ -0,0 +1,19 @@
+                minlen=min(minlen,len);
+                sum-=nums[i];
+
+                i++;
+                len=j-i+1;
+            while(sum>=target){
+            sum+=nums[j];
+
+            }
+            j++;
+        while(j<n){
+        int sum=0;
+
+        }
+        return minlen;
+        
+    }
+        if(minlen==n+1) return 0;
+
