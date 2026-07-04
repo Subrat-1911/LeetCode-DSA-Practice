86b6ec8c7334767d25b3ecd57ebc3a3b166e4513
Subrat Sahoo <subratsahoo28032006@gmail.com>
2026-07-04 07:21:50 +0530

    Auto-sync: Code for 238. Product of Array Except Self

--- 238.product-of-array-except-self/238.product-of-array-except-self.cpp
+++ 238.product-of-array-except-self/238.product-of-array-except-self.cpp
@@ -1,18 +1,20 @@
+public:
+    vector<int> productExceptSelf(vector<int>& nums) {
+        int n=nums.size();
+        vector<int> pre(n);
+        pre[0]=1;
+        for(int i=1;i<n;i++){
+        
+            pre[i]=pre[i-1]*nums[i-1];
         }
-        int p =nums[n-1];
+        int prod=nums[n-1];
         for(int i=n-2;i>=0;i--){
-            p*=nums[i];
+            pre[i]=pre[i]*prod;
         }
+    }
+
+            prod=prod*nums[i];
         return pre;
 
-        
-        
-
-    }
-            pre[i]=pre[i-1]*nums[i-1];
-        for(int i=1;i<n;i++){
-        pre[0]=1;
-            pre[i]*=p;
-        vector<int>pre(n);
-
+};
 
