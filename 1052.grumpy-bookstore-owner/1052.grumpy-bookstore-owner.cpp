86a449a2bf60688191a62e803423be3d1bf059b6
Subrat Sahoo <subratsahoo28032006@gmail.com>
2026-07-04 19:58:40 +0530

    Auto-sync: Code for 1052. Grumpy Bookstore Owner

---
+++ 1052.grumpy-bookstore-owner/1052.grumpy-bookstore-owner.cpp
@@ -0,0 +1,32 @@
+                maxidx=i;
+            }
+            preWindow=currWindow;
+                maxSum=currWindow;
+            if(maxSum<currWindow){
+            }
+            int currWindow=preWindow+nxtWin-preWin;
+                preWin=customers[i-1];
+            int preWin=0;
+            if(grumpy[i-1]==1){
+            }
+            if(grumpy[j]==1){
+                nxtWin=customers[j];
+            int nxtWin=0;
+        int maxidx=0;
+        while(j<n){
+            i++;
+            j++;
+
+        }
+        for(int i=maxidx;i<maxidx+minutes;i++){
+            grumpy[i]=0;
+
+        }
+        int answer=0;
+        for(int i=0;i<n;i++){
+            if(grumpy[i]==0){
+               answer += customers[i];
+            }
+            
+        }
+
