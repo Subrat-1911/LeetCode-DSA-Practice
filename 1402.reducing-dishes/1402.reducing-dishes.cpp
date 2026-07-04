2f3ecdaab0fded6985311292632aed959f434f7b
Subrat Sahoo <subratsahoo28032006@gmail.com>
2026-07-04 09:05:58 +0530

    Auto-sync: Code for 1402. Reducing Dishes

--- 1402.reducing-dishes/1402.reducing-dishes.cpp
+++ 1402.reducing-dishes/1402.reducing-dishes.cpp
@@ -1,14 +1,14 @@
         for(int i=0;i<n;i++){
             if(suf[i]>=0){
         }
-                mindex=i;
+                mindex=i;  // mark the idx
             }
-        int mindex=-1;
-        for(int i=mindex;i<n;i++){
-        int sat=1;
-            sum=sum+(satisfaction[i]*sat);
+        int mindex=-1; //check where +ve number is starting 
+        for(int i=mindex;i<n;i++){ //run loop start with mindex till end
+        int sat=1; //satisfation level
+            sum=sum+(satisfaction[i]*sat);  // add in sum by multiply with satisfaction level
         long sum=0;
-            sat++;
+            sat++;  //satisfaction level alwayes increase by 1;
         }
             suf[i]=satisfaction[i]+suf[i+1];
         for(int i=n-2;i>=0;i--){
@@ -17,7 +17,11 @@
         return sum;
 
                 break;
-        if(mindex==-1) return 0;
-    }
-};
+        if(mindex==-1) return 0; // if there is no +ve number return 0;
+        long suf[n]; //then create an suffix sum array
+        sort(satisfaction.begin(),satisfaction.end()); //1st sort the array
+    int maxSatisfaction(vector<int>& satisfaction) {
+        int n = satisfaction.size();
+public:
+class Solution {
 
