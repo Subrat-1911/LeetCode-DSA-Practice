a2860684bfbc94fe691ae22a8a486aa55ef5e06c
Subrat Sahoo <subratsahoo28032006@gmail.com>
2026-07-05 19:37:45 +0530

    Auto-sync: Code for 23. Merge k Sorted Lists

--- 23.merge-k-sorted-lists/23.merge-k-sorted-lists.cpp
+++ 23.merge-k-sorted-lists/23.merge-k-sorted-lists.cpp
@@ -1,18 +1,18 @@
-                tail=tail->next;
+        //     lists.pop_back();
+        //     ListNode* b=lists[lists.size()-1];
+        //     lists.pop_back();
+        //     ListNode* c=mergeTwoLists(a,b);
+        //     lists.push_back(c);
 
-            }
-            tail->next=(list1!=NULL)?list1:list2;
-            
-        }
-        return dummy->next;
-    }
-    ListNode* mergeKLists(vector<ListNode*>& lists) {   // in this vector we pop back 2 node and sent to merge and push 
-    merge list;
-        if(lists.size()==0) return NULL;
-        while(lists.size()>1){
-            ListNode* a=lists[lists.size()-1];
-            lists.pop_back();
-            ListNode* b=lists[lists.size()-1];
-            lists.pop_back();
+        // }
+        while(lists.size()>1){        //TC=O(NKlogk)
+            ListNode* a=lists[0];  // starting se 2 node uthao then merge then push back
+            lists.erase(lists.begin());  // use to remove 1st element of vector
+            ListNode* b=lists[0];
+            lists.erase(lists.begin());
             ListNode* c=mergeTwoLists(a,b);
+            lists.push_back(c);
+
+        }
+        return lists[0];  // return the 1st element;
 
