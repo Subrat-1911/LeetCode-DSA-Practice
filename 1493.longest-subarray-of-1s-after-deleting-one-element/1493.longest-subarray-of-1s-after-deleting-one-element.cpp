c4853fcbe545985d00da50bbc80c5a1c4fb07ee7
Subrat Sahoo <subratsahoo28032006@gmail.com>
2026-07-05 18:10:34 +0530

    Auto-sync: Code for 1493. Longest Subarray of 1's After Deleting One Element

---
+++ 1493.longest-subarray-of-1s-after-deleting-one-element/1493.longest-subarray-of-1s-after-deleting-one-element.cpp
@@ -0,0 +1,28 @@
+                else{  //j=0 & del=1;
+                    len=j-i;
+                    maxlen=max(maxlen,len);
+                    while(nums[i]==0)i++;
+                }
+            }
+        }
+        len=j-i;
+                }
+                    j++;
+                    del++;
+                if(del<1){
+            else {   //j=0
+            if(nums[j]==1) j++;
+        while(j<n){
+        int maxlen=INT_MIN;
+                    j++;
+        maxlen=max(maxlen,len);
+        return maxlen-1;
+    }
+        int n =nums.size();
+        int i=0;
+        int j=0;
+        int len=0;
+        int del=0;
+                    i++;
+};
+
