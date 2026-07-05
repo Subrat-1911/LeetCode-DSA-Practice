1d33830bcb5613422d6f00b2bd752a61ea0caacc
Subrat Sahoo <subratsahoo28032006@gmail.com>
2026-07-05 19:09:36 +0530

    Auto-sync: Code for 21. Merge Two Sorted Lists

--- 21.merge-two-sorted-lists/21.merge-two-sorted-lists.cpp
+++ 21.merge-two-sorted-lists/21.merge-two-sorted-lists.cpp
@@ -1,23 +1,18 @@
- * };
- */
-class Solution {
+        if(list1!=NULL && list2==NULL) return list1;
+
+        if(list1==NULL && list2==NULL) return NULL;
+        if(list1==NULL && list2!=NULL) return list2;
 public:
     ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
-        ListNode* dummy= new ListNode(0);
-        ListNode* tail=dummy;
-        while(list1!=0 && list2!=0){
-            if(list1->val<list2->val){
-                tail->next=list1;
-            }
-                list1=list1->next;
-                tail=tail->next;
-            else{
-
-                tail->next=list2;
-                list2=list2->next;
-                tail=tail->next;
-        if(list1==NULL && list2==NULL) return NULL;
-
-        if(list1==NULL && list2!=NULL) return list2;
-        if(list1!=NULL && list2==NULL) return list1;
+ */
+class Solution {
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ * struct ListNode {
+ *     int val;
+ * Definition for singly-linked list.
+/**
 
