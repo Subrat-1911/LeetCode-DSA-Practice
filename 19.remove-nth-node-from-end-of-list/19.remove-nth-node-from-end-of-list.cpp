2b9865817da0268888b81453569185b015e6c43a
Subrat Sahoo <subratsahoo28032006@gmail.com>
2026-07-06 11:59:06 +0530

    Auto-sync: Code for 19. Remove Nth Node From End of List

---
+++ 19.remove-nth-node-from-end-of-list/19.remove-nth-node-from-end-of-list.cpp
@@ -0,0 +1,20 @@
+        ListNode* temp=head;
+        int k=0;
+        while(temp!=NULL){
+        if(head->next == NULL && n==1) return NULL;
+        if(head==NULL) return NULL;
+            temp=temp->next;
+            k++;
+        }
+        int m=k-n;
+        temp=head;
+        for(int i=0;i<m-1;i++){
+            temp=temp->next;
+        }
+        temp->next=temp->next->next;
+        return head;
+        
+    }
+        if(head->next->next == NULL && n==2) return head->next;
+};
+
