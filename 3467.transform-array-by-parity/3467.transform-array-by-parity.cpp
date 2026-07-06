2f47de5aee407433804876fdf55a52c585f588a8
Subrat Sahoo <subratsahoo28032006@gmail.com>
2026-07-06 08:31:45 +0530

    Auto-sync: Code for 3467. Transform Array by Parity

--- 3467.transform-array-by-parity/3467.transform-array-by-parity.cpp
+++ 3467.transform-array-by-parity/3467.transform-array-by-parity.cpp
@@ -1,15 +1,19 @@
-class Solution {
+                nums[j]=1;
+            else {
+                nums[i]=0;
+            else if(nums[i]%2!=0 && nums[j]%2!=0){
+                nums[j]=1;
+                j--;
+            }
+            }
+                nums[i]=0;
+                i++;
+        while(i<=j){
+            if(nums[i]%2==0 && nums[j]%2==0){
+        int i=0;
+        int j=n-1;
 public:
     vector<int> transformArray(vector<int>& nums) {
         int n=nums.size();
-        for(int i=0;i<n;i++){
-            if(nums[i]%2==0){
-                nums[i]=0;
-            }
-        }
-    }
-            else nums[i]=1;
-        sort(nums.begin(),nums.end());
-        return nums;
-};
+class Solution {
 
