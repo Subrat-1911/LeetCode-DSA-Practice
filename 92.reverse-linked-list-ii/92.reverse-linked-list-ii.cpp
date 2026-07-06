3aef11b7b8b357c98d109a412069202e3d74c0ab
Subrat Sahoo <subratsahoo28032006@gmail.com>
2026-07-06 10:00:28 +0530

    Auto-sync: Code for 92. Reverse Linked List II

--- 92.reverse-linked-list-ii/92.reverse-linked-list-ii.cpp
+++ 92.reverse-linked-list-ii/92.reverse-linked-list-ii.cpp
@@ -1,17 +1,17 @@
-            if(n==left-1)a=temp;
             else if(n==left) b=temp;
             else if(n==right) c=temp;
             else if(n==right+1) d=temp;
             temp=temp->next;
             n++;
         }
-        a->next=NULL;
+        if(a!=NULL) a->next=NULL;
         c->next=NULL;
         c=Reverse(b);
-        a->next=c;
+        if(a!=NULL) a->next=c;
         b->next=d;
+        if(a!=NULL) return head;
 
     }
-        return head;
+        else return c;
 };
 
