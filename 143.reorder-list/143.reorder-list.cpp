96b65dddd34be4675566576f11aca333fb1f8eaa
Subrat Sahoo <subratsahoo28032006@gmail.com>
2026-07-06 10:57:54 +0530

    Auto-sync: Code for 143. Reorder List

---
+++ 143.reorder-list/143.reorder-list.cpp
@@ -0,0 +1,19 @@
+        while(a!=NULL && b!=NULL){
+            c->next=a;
+        ListNode* a=head;
+            c=c->next;
+            a=a->next;
+            c->next=b;
+            c=c->next;
+            b=b->next;
+
+        ListNode* b=newHead;
+        }
+        c->next=a;
+        head=dummy->next;
+    }
+};
+        ListNode* c=dummy;
+        ListNode* dummy=new ListNode(100);
+        newHead=Reverse(newHead);
+
