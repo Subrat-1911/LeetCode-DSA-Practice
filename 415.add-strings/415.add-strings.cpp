2e87dd39d93b5dd82592f2a0f5d14902e63edaaf
Subrat Sahoo <subratsahoo28032006@gmail.com>
2026-07-06 12:39:36 +0530

    Auto-sync: Code for 415. Add Strings

--- 415.add-strings/415.add-strings.cpp
+++ 415.add-strings/415.add-strings.cpp
@@ -1,14 +1,19 @@
 class Solution {
 public:
     string addStrings(string num1, string num2) {
-        long long x=stoll(num1);
-        long long y=stoll(num2);
-
-        long long sum =x+y;
-        string s=std::to_string(sum);
-
-        return s;
+        int i = num1.size()-1;
+        int j = num2.size()-1;
+        int carry=0;
+        string ans="";
+        while(i>=0 || j>=0 || carry){
+            int sum=carry;
+        }
     }
-   
-};
+            if(i>=0) sum+=num1[i--]-'0';
+            if(j>=0) sum+=num2[j--]-'0';
+
+            ans.push_back((sum%10)+'0');
+            carry=sum/10;
+        return ans;
+        reverse(ans.begin(),ans.end());
 
