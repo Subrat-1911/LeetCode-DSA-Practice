7edbbc662d6ce473eac454ab66b204b6d70904f8
Subrat Sahoo <subratsahoo28032006@gmail.com>
2026-07-09 19:43:50 +0530

    Auto-sync: Code for 543. Diameter of Binary Tree

--- 543.diameter-of-binary-tree/543.diameter-of-binary-tree.cpp
+++ 543.diameter-of-binary-tree/543.diameter-of-binary-tree.cpp
@@ -1,19 +1,21 @@
+    }
+    void helper(TreeNode* root, int& maxDia){
 
-class Solution {
-public:
-    int NoOfLevel(TreeNode* root){
-        if(root==NULL) return 0;
-        int maxLevel=1+max(NoOfLevel(root->left),NoOfLevel(root->right));
-        return maxLevel;
+        if(root==NULL) return;
+        int Dia = NoOfLevel(root->left) + NoOfLevel(root->right);    
+        maxDia=max(maxDia,Dia);
+        helper(root->left,maxDia); 
+        helper(root->right,maxDia);
+        
     }
     int diameterOfBinaryTree(TreeNode* root) {
-        maxDia=max(maxDia,Dia);
-    }
+        int maxDia=0;
+        helper(root,maxDia);
+        return maxLevel;
+        int maxLevel=1+max(NoOfLevel(root->left),NoOfLevel(root->right));
         if(root==NULL) return 0;
-        int Dia = NoOfLevel(root->left) + NoOfLevel(root->right);
+    int NoOfLevel(TreeNode* root){
     int maxDia=0;
-        diameterOfBinaryTree(root->left);
-        diameterOfBinaryTree(root->right);
-        return maxDia;
-};
+public:
+class Solution {
 
