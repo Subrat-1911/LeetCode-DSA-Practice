d31f92813a57e592df668b52204f415a8d3b6cfa
Subrat Sahoo <subratsahoo28032006@gmail.com>
2026-07-10 13:35:13 +0530

    Auto-sync: Code for 103. Binary Tree Zigzag Level Order Traversal

--- 103.binary-tree-zigzag-level-order-traversal/103.binary-tree-zigzag-level-order-traversal.cpp
+++ 103.binary-tree-zigzag-level-order-traversal/103.binary-tree-zigzag-level-order-traversal.cpp
@@ -1,19 +1,28 @@
         }
-            PrintNthLevel(root->right,currLevel,tarLevel,subans);
-            PrintNthLevel(root->left,currLevel,tarLevel,subans);
+            PrintNthLevel(root->right,currLevel+1,tarLevel,subans);
+            PrintNthLevel(root->left,currLevel+1,tarLevel,subans);
         else {
+        
+    }
+    void printLevelWise(TreeNode* root,vector<vector<int>>& ans){
+        int n=NoOfLevel(root);
         }
-            PrintNthLevel(root->left,currLevel,tarLevel,subans);
-            PrintNthLevel(root->right,currLevel,tarLevel,subans);
-        if(currLevel%2==0){
-        }
-            return ;
-            subans.push_back(root->val);
+            PrintNthLevel(root->left,currLevel+1,tarLevel,subans);
+        if(tarLevel%2==0){
+            PrintNthLevel(root->right,currLevel+1,tarLevel,subans);
+        for(int i=1;i<=n;i++){
+            vector<int> subans;
+            PrintNthLevel(root,1,i,subans);
         if(currLevel==tarLevel){
+            subans.push_back(root->val);
+            return ;
+        }
+
         if(root==NULL) return;
    void PrintNthLevel(TreeNode* root, int currLevel, int tarLevel,vector<int>& subans){
     }
         return maxLevel;
         int maxLevel=1+max(NoOfLevel(root->left),NoOfLevel(root->right));
-        currLevel+=1;
+        if(root==NULL) return 0;
+            ans.push_back(subans);
 
