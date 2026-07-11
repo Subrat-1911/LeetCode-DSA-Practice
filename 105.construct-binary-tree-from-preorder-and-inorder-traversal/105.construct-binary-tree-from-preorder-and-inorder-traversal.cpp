048bc7c7db4a2b76aed5f6f71f2c8b998d9cdaa5
Subrat Sahoo <subratsahoo28032006@gmail.com>
2026-07-11 20:23:30 +0530

    Auto-sync: Code for 105. Construct Binary Tree from Preorder and Inorder Traversal

--- 105.construct-binary-tree-from-preorder-and-inorder-traversal/105.construct-binary-tree-from-preorder-and-inorder-traversal.cpp
+++ 105.construct-binary-tree-from-preorder-and-inorder-traversal/105.construct-binary-tree-from-preorder-and-inorder-traversal.cpp
@@ -1,18 +1,18 @@
         }
-        int lcount=i-prelo;
-        int rcount=prehi-i;
+        int lcount=i-inlo;
+        int rcount=inhi-i;
         root->left=build(pre,prelo+1,prelo+lcount,in,inlo,i-1);
         root->right=build(pre,prelo+lcount+1,prehi,in,i+1,inhi);
             i++;
-        while(i<=inhi){
             if(pre[prelo]==in[i])break;
-        TreeNode* root=new TreeNode(pre[prelo]);
-        if(prelo==prehi) return root;
         int i=inlo;
-    TreeNode* build(vector<int>& pre, int prelo, int prehi, vector<int>& in, int inlo, int inhi){
+        while(i<=inhi){
+        if(prelo==prehi) return root;
+        TreeNode* root=new TreeNode(pre[prelo]);
         if(prelo>prehi) return NULL;
         return root;
     }
     TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
-        int n=preorder.size();
+    TreeNode* build(vector<int>& pre, int prelo, int prehi, vector<int>& in, int inlo, int inhi){
+public:
 
