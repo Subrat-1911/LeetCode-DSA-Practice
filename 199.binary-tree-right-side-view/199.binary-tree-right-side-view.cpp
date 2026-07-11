63b89d1288509f15469046dd1fcceb6a9f3da16b
Subrat Sahoo <subratsahoo28032006@gmail.com>
2026-07-11 19:03:37 +0530

    Auto-sync: Code for 199. Binary Tree Right Side View

--- 199.binary-tree-right-side-view/199.binary-tree-right-side-view.cpp
+++ 199.binary-tree-right-side-view/199.binary-tree-right-side-view.cpp
@@ -1,18 +1,9 @@
-        }
-        PrintNthLevel(root->left,currLevel+1,tarLevel,ans);
-        PrintNthLevel(root->right,currLevel+1,tarLevel,ans);
-    }
-    void printLevelWise(TreeNode* root,vector<int>& ans){
-        int n=ans.size();
-        for(int i=0;i<n;i++){
-            PrintNthLevel(root,0,i,ans);
-            ans[currLevel]=root->val;
-        if(currLevel==tarLevel){
-        if(root==NULL) return;
-    void PrintNthLevel(TreeNode* root, int currLevel, int tarLevel,vector<int>& ans){
     }
         return maxLevel;
         int maxLevel=1+max(NoOfLevel(root->left),NoOfLevel(root->right));
         if(root==NULL) return 0;
     int NoOfLevel(TreeNode* root){
+public:
+class Solution {
+
 
