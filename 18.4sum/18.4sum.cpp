9114eccfae2ca60f5a5df83520370d616582ee78
Subrat Sahoo <subratsahoo28032006@gmail.com>
2026-07-12 20:57:10 +0530

    Auto-sync: Code for 18. 4Sum

--- 18.4sum/18.4sum.cpp
+++ 18.4sum/18.4sum.cpp
@@ -1,22 +1,19 @@
                         hi--;
-                        
-                        while(lo<hi && nums[lo]==nums[lo-1]){
-                            lo++;
-                        }
-                        while(lo<hi  && nums[hi]==nums[hi+1]){
-                            hi--;
-                            
-                        }
-                    }
-                    else if(s+k+nums[lo]+nums[hi]>target){
-                        hi--;
-                    }
-                    else lo++;
-                }
-
-            }
-            
-            
-            
-        }
+                        lo++;
+                        ans.push_back(v);
+                        v.push_back(nums[hi]);
+                        v.push_back(nums[lo]);
+                        v.push_back(k);
+                        v.push_back(s);
+                    if(sum==target){
+                    long long sum=(s+k+nums[lo]+nums[hi]);
+                    vector<int>v;
+                while(lo<hi){
+                long long hi=n-1;
+                long long lo =j+1;
+                long long k=nums[j];
+                long long s=nums[i];
+                if(j>i+1 && nums[j]==nums[j-1]) continue;
+            for(int j=i+1; j<n-2;j++){
+            if(i>0 && nums[i]==nums[i-1]) continue;
 
