2787dd2dcd85b132b2e87176383660a5de9663f7
Subrat Sahoo <subratsahoo28032006@gmail.com>
2026-07-13 19:24:47 +0530

    Auto-sync: Code for 16. 3Sum Closest

---
+++ 16.3sum-closest/16.3sum-closest.cpp
@@ -0,0 +1,23 @@
+            }
+                if(abs(sum-target)<abs(closet-target)){
+                    closet=sum;
+                }
+                if(sum==target) return target;
+            
+        }
+                int sum=s+nums[lo]+nums[hi];
+            while(lo<hi){
+            closet=s+nums[lo]+nums[hi];
+            int hi=n-1;
+            int lo =i+1;
+            int s=nums[i];
+        for(int i=0;i<n-2;i++){
+        int closet=0;
+        int n=nums.size();
+        sort(nums.begin(),nums.end());
+        return closet;
+         
+        
+                lo++;
+                hi--;
+
