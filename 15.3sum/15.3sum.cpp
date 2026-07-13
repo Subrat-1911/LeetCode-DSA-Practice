a9190c9074fab35f28115a98ddfbc8f8bb4e2931
Subrat Sahoo <subratsahoo28032006@gmail.com>
2026-07-13 19:35:45 +0530

    Auto-sync: Code for 15. 3Sum

--- 15.3sum/15.3sum.cpp
+++ 15.3sum/15.3sum.cpp
@@ -1,21 +1,18 @@
-                    while(lo<hi && nums[lo]==nums[lo-1]){
-                    
-                        lo++;
-                    }
-                    hi--;
-                    ans.push_back(v);
-                    lo++;
-                if(s+nums[lo]+nums[hi]==0){
-                    v.push_back(s);
                     v.push_back(nums[lo]);
                     v.push_back(nums[hi]);
-                    while(lo<hi  && nums[hi]==nums[hi+1]){
-                        hi--;
-                        
-                    }
-                }
-                else if(s+nums[lo]+nums[hi]>0){
-                    hi--;
-                }
-                else lo++;
+            if(i>0 && nums[i]==nums[i-1]) continue;
+            int s=nums[i];
+            int lo =i+1;
+            int hi=n-1;
+            while(lo<hi){
+                vector<int>v;
+                if(s+nums[lo]+nums[hi]==0){
+                    v.push_back(s);
+class Solution {
+public:
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        vector<vector<int>>ans;
+        sort(nums.begin(),nums.end());
+        int n=nums.size();
+        for(int i=0;i<n-2;i++){
 
