d1274acd3b8ceb2a2dd7928cc62327d38df7a832
Subrat Sahoo <subratsahoo28032006@gmail.com>
2026-07-13 20:42:27 +0530

    Auto-sync: Code for 645. Set Mismatch

---
+++ 645.set-mismatch/645.set-mismatch.cpp
@@ -0,0 +1,18 @@
+            if(nums[i]==nums[i+1]){
+        }
+                dup=nums[i];
+            }
+                break;
+        for(int i=0;i<n;i++){
+            sum+=nums[i];
+        int sum=0;
+        ans.push_back(dup);
+        }
+        int orgSum=n*(n+1)/2;
+        int mis=(orgSum-(sum-dup));
+        ans.push_back(mis);
+        return ans;
+
+    }
+};
+
