9d96c42480b1ac96a77fd0e84bfbc637290c33fa
Subrat Sahoo <subratsahoo28032006@gmail.com>
2026-07-14 19:00:17 +0530

    Auto-sync: Code for 1402. Reducing Dishes

--- 1402.reducing-dishes/1402.reducing-dishes.cpp
+++ 1402.reducing-dishes/1402.reducing-dishes.cpp
@@ -1,19 +1,18 @@
                 break;
             }
         }
-        if(mindex==-1) return 0; // if there is no +ve number return 0;
                 mindex=i;  // mark the idx
+        int mindex=-1; //check where +ve number is starting 
         for(int i=0;i<n;i++){
             if(suf[i]>=0){
         for(int i=n-2;i>=0;i--){
             suf[i]=satisfaction[i]+suf[i+1];
         }
-        int mindex=-1; //check where +ve number is starting 
-        suf[n-1]=satisfaction[n-1];
-        long suf[n]; //then create an suffix sum array
         int n = satisfaction.size();
         sort(satisfaction.begin(),satisfaction.end()); //1st sort the array
-    int maxSatisfaction(vector<int>& satisfaction) {
-public:
+        long suf[n]; //then create an suffix sum array
+        suf[n-1]=satisfaction[n-1];
 class Solution {
+public:
+    int maxSatisfaction(vector<int>& satisfaction) {
 
