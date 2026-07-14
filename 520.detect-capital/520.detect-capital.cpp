b48a1ca8ab46b1163f0ea466a8a61b2bde9ee858
Subrat Sahoo <subratsahoo28032006@gmail.com>
2026-07-14 19:01:27 +0530

    Auto-sync: Code for 520. Detect Capital

--- 520.detect-capital/520.detect-capital.cpp
+++ 520.detect-capital/520.detect-capital.cpp
@@ -1,14 +1,19 @@
+            if(word[i]<='Z'){
+                count++;
+            }
+        }
         if(count==1){
             if(word[0]<='Z'){
                 return true;
-        }
             }
-                count++;
-            if(word[i]<='Z'){
+            else return false;
+        }
+        if(count==0 || count == n) return true;
         for(int i=0;i<n;i++){
         int count=0;
         int n=word.size();
     bool detectCapitalUse(string word) {
-public:
-class Solution {
+        else return false;
+    }
+};
 
