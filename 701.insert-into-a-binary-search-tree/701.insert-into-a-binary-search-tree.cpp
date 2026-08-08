681f91ec91fcee1f7232d3342922dd39049ce298
Subrat Sahoo <subratsahoo28032006@gmail.com>
2026-08-08 16:55:17 +0530

    Auto-sync: Code for 701. Insert into a Binary Search Tree

--- 701.insert-into-a-binary-search-tree/701.insert-into-a-binary-search-tree.cpp
+++ 701.insert-into-a-binary-search-tree/701.insert-into-a-binary-search-tree.cpp
@@ -1,17 +1,17 @@
-            }
-        }
-        else if(root->val>val){
-            if(root->left != NULL){
-                insertIntoBST(root->left,val);
-                return root;
-            }
-            else{
-                TreeNode *l= new TreeNode(val);
-                root->left=l;
-                return root;
                 root->right=r;
                 TreeNode *r= new TreeNode(val);
             else{
             }
                 insertIntoBST(root->right,val);
+            if(root->right != NULL){
+        if(root->val<val){
+        }
+            return root;
+           TreeNode *root= new TreeNode(val);
+        if(root == NULL){
+    TreeNode* insertIntoBST(TreeNode* root, int val) {
+public:
+class Solution {
+ */
+ * };
 
