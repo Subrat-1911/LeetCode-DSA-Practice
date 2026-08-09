1b3c61bcac6faf225449fff0318605c608ebaa36
Subrat Sahoo <subratsahoo28032006@gmail.com>
2026-08-09 15:11:55 +0530

    Auto-sync: Code for 98. Validate Binary Search Tree

--- 98.validate-binary-search-tree/98.validate-binary-search-tree.cpp
+++ 98.validate-binary-search-tree/98.validate-binary-search-tree.cpp
@@ -1,13 +1,16 @@
-
-        return min(root->val,min(minTree(root->left),minTree(root->right)));
         
+
     }
+        return min(root->val,min(minTree(root->left),minTree(root->right)));
+        if(root==NULL) return INT_MAX;
+    int minTree(TreeNode* root){
+    }
+        
     bool isValidBST(TreeNode* root) {
-       if(maxTree(root->left)>root->val) return false;
-       else if(minTree(root->right)<root->val) return false;
+        if(root==NULL) return true;
+       if(maxTree(root->left)>=root->val) return false;
+       else if(minTree(root->right)<=root->val) return false;
        return isValidBST(root->left) && isValidBST(root->right);
 
     }
-        if(root==NULL) return true;
-};
 
