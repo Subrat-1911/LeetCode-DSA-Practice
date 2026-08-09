8b8f9270a3193a6056ccfe55dd3f95da1077a4ef
Subrat Sahoo <subratsahoo28032006@gmail.com>
2026-08-09 15:22:37 +0530

    Auto-sync: Code for 94. Binary Tree Inorder Traversal

--- 94.binary-tree-inorder-traversal/94.binary-tree-inorder-traversal.cpp
+++ 94.binary-tree-inorder-traversal/94.binary-tree-inorder-traversal.cpp
@@ -1,22 +1,22 @@
-class Solution {
-public:
     vector<int> inorderTraversal(TreeNode* root) {
         vector<int>ans;
         TreeNode* node=root;
+        stack<TreeNode*> st;
         while(st.size()>0 || node!=NULL){
             if(node!=NULL){
-        }
-        stack<TreeNode*> st;
                 st.push(node);
-            }
                 node=node->left;
+            }
             else{
                 TreeNode* temp=st.top();
-            }
-    }
                 st.pop();
                 ans.push_back(temp->val);
                 node=temp->right;
+            }
+        }
         return ans;
+    }
 };
+public:
+class Solution {
 
