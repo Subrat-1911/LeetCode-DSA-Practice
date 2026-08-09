e1895f93218b4b908997e066680e97be53ad4ad9
Subrat Sahoo <subratsahoo28032006@gmail.com>
2026-08-09 15:26:00 +0530

    Auto-sync: Code for 98. Validate Binary Search Tree

--- 98.validate-binary-search-tree/98.validate-binary-search-tree.cpp
+++ 98.validate-binary-search-tree/98.validate-binary-search-tree.cpp
@@ -1,12 +1,20 @@
-        
+    void inorderTraversal(TreeNode* root,vector<int>& ans){
+        if(root==NULL) return;
+        inorderTraversal(root->left,ans);
+        ans.push_back(root->val);
+        inorderTraversal(root->right,ans);
 
     }
+    
+public:
     bool isValidBST(TreeNode* root) {
-        if(root==NULL) return true;
-       if(maxTree(root->left)>=root->val) return false;
-       else if(minTree(root->right)<=root->val) return false;
-       return isValidBST(root->left) && isValidBST(root->right);
+        vector<int> ans;
 
+        inorderTraversal(root, ans);
+        for(int i=1;i<ans.size();i++){
+            if(ans[i]<=ans[i-1]) return false;
+        }
     }
+        return true;
 };
 
