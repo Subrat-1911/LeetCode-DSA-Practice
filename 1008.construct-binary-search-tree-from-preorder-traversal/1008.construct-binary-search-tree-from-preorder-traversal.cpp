1d782f09990071f9b0a27830e40dc5934b2a11cf
Subrat Sahoo <subratsahoo28032006@gmail.com>
2026-08-09 16:08:56 +0530

    Auto-sync: Code for 1008. Construct Binary Search Tree from Preorder Traversal

---
+++ 1008.construct-binary-search-tree-from-preorder-traversal/1008.construct-binary-search-tree-from-preorder-traversal.cpp
@@ -0,0 +1,25 @@
+                root->right=r;
+                TreeNode *r= new TreeNode(val);
+            }
+            else{
+        if(root->val<val){
+            if(root->right != NULL){
+                insert(root->right,val);
+           TreeNode *root= new TreeNode(val);
+        }
+        if(root == NULL){
+    void insert(TreeNode *root, int val){
+public:
+class Solution {
+ */
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode *left;
+ * struct TreeNode {
+ *     int val;
+ * Definition for a binary tree node.
+/**
+
