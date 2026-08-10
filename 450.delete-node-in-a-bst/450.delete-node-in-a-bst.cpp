4ac1d8ebf003a8c52a3755241bdb1193cbd66670
Subrat Sahoo <subratsahoo28032006@gmail.com>
2026-08-10 18:09:07 +0530

    Auto-sync: Code for 450. Delete Node in a BST

--- 450.delete-node-in-a-bst/450.delete-node-in-a-bst.cpp
+++ 450.delete-node-in-a-bst/450.delete-node-in-a-bst.cpp
@@ -1,17 +1,22 @@
+        if(root->right==NULL) return NULL;
+        TreeNode* suc=root->left;
+        while(suc->right!=NULL){
+            suc=suc->right;
+        }
+        return suc;
+            pred=pred->right;
+        }
+        return pred;
+    }
+    TreeNode* ios(TreeNode* root){  // to find suc
+    TreeNode* iop(TreeNode* root){   // to find predi
+        if(root->left==NULL) return NULL;
+        TreeNode* pred=root->left;
+        while(pred->right!=NULL){
+class Solution {
+public:
 
-        }
-        else if(root->val<key){ //go right
-            root->right=deleteNode(root->right,key);
-
-        }
-        else{ //go left
-            root->left=deleteNode(root->left,key);
-        }
-        return root;
-            }
-                root->left=deleteNode(root->left,pred->val);  // now target is pred->val and it placed in left of 
-                //delete, for delete use same function
-                root->val=pred->val;  // replace
-                TreeNode* pred=iop(root);
-                root
+    }
+    TreeNode* deleteNode(TreeNode* root, int key) {
+        if(root==NULL) return NULL;
 
