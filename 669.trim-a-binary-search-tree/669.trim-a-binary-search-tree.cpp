fe7bd115ae45bd574c88bde311341cd13cc9a6d7
Subrat Sahoo <subratsahoo28032006@gmail.com>
2026-08-16 14:44:15 +0530

    Auto-sync: Code for 669. Trim a Binary Search Tree

---
+++ 669.trim-a-binary-search-tree/669.trim-a-binary-search-tree.cpp
@@ -0,0 +1,17 @@
+            if(root->right->val<lo){
+        while(root->right!=NULL){
+        }
+            else break;
+            }
+                root->left=root->left->left;
+            else if(root->left->val>hi){
+            }
+            if(root->left->val<lo){
+                root->left=root->left->right;
+        while(root->left!=NULL){
+        if(root==NULL) return;
+    void trim(TreeNode* root, int lo, int hi){
+class Solution {
+public:
+
+
