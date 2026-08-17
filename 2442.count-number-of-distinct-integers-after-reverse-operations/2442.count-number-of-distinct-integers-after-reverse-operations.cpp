b45c7185cd35a16a3a8dd209245dd124200f683f
Subrat Sahoo <subratsahoo28032006@gmail.com>
2026-08-17 18:50:11 +0530

    Auto-sync: Code for 2442. Count Number of Distinct Integers After Reverse Operations

---
+++ 2442.count-number-of-distinct-integers-after-reverse-operations/2442.count-number-of-distinct-integers-after-reverse-operations.cpp
@@ -0,0 +1,20 @@
+        }
+        int ans=s.size();
+            while(nums[i]>0){
+                int x = nums[i]%10;
+                rev=rev*10+x;
+                nums[i]/=10;
+            }
+    }
+            s.insert(rev);
+            int rev=0;
+            s.insert(nums[i]);
+        int n = nums.size();
+        for(int i=0;i<n;i++){
+        return ans;
+};
+        unordered_set<int> s;
+    int countDistinctIntegers(vector<int>& nums) {
+public:
+class Solution {
+
