a1cc23f0054efaeaeb888304e4925a95bed69695
Subrat Sahoo <subratsahoo28032006@gmail.com>
2026-08-18 10:01:55 +0530

    Auto-sync: Code for 1207. Unique Number of Occurrences

---
+++ 1207.unique-number-of-occurrences/1207.unique-number-of-occurrences.cpp
@@ -0,0 +1,18 @@
+        }
+        for(auto freq:mp){
+            int f=freq.second;
+        }
+            if(s.find(f)!=s.end()){ // means freq found
+        unordered_set<int>s;
+                return false;
+            }
+            else{
+                s.insert(f);
+            }
+        return true;
+    }
+};
+            mp[arr[i]]++;
+        for(int i=0;i<n;i++){
+        unordered_map<int,int>mp;
+
