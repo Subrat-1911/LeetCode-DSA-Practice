27dd0f2b4138e01d69e7ee5c633deb643d00e784
Subrat Sahoo <subratsahoo28032006@gmail.com>
2026-08-20 19:00:49 +0530

    Auto-sync: Code for 1512. Number of Good Pairs

---
+++ 1512.number-of-good-pairs/1512.number-of-good-pairs.cpp
@@ -0,0 +1,15 @@
+        int n=nums.size();
+        for(int i=0;i<n-1;i++){
+            for(int j=i+1;j<n;j++){
+                if(nums[i]==nums[j]){
+            }
+        }
+                    if(i<j){
+                }
+                        count++;
+                    }
+    }
+        int count=0;
+        return count;
+};
+
