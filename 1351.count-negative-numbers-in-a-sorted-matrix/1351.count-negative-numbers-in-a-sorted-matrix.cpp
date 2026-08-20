25b80c6a306cc583bd5a0abf475eb4417d9123c3
Subrat Sahoo <subratsahoo28032006@gmail.com>
2026-08-20 11:05:48 +0530

    Auto-sync: Code for 1351. Count Negative Numbers in a Sorted Matrix

---
+++ 1351.count-negative-numbers-in-a-sorted-matrix/1351.count-negative-numbers-in-a-sorted-matrix.cpp
@@ -0,0 +1,14 @@
+public:
+    int countNegatives(vector<vector<int>>& grid) {
+        int m = grid.size();
+        int n = grid[0].size();
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+        }
+                if(grid[i][j]<0) count++;
+            }
+    }
+        int count=0;
+        return count;
+};
+
