0a4cc163c0631f73e936ef9c1859901694d9281e
Subrat Sahoo <subratsahoo28032006@gmail.com>
2026-08-20 20:03:34 +0530

    Auto-sync: Code for 3386. Button with Longest Push Time

---
+++ 3386.button-with-longest-push-time/3386.button-with-longest-push-time.cpp
@@ -0,0 +1,18 @@
+        for(int i=0;i<n;i++){
+            if(nums[i]>max){
+        int max=INT_MIN;
+        int maxidx=-1;
+                max=nums[i];
+        }
+            nums.push_back(sub);
+                maxidx=i;
+            }
+
+        }
+        return events[maxidx][0];
+        
+    }
+            int sub= events[i-1][1]-events[i-1][1];
+        for(int i=1;i<n;i++){
+
+
