e8e1edc6515847e797e1734a6d6cc64ce3a1e8b1
Subrat Sahoo <subratsahoo28032006@gmail.com>
2026-08-20 11:00:45 +0530

    Auto-sync: Code for 2529. Maximum Count of Positive Integer and Negative Integer

--- 2529.maximum-count-of-positive-integer-and-negative-integer/2529.maximum-count-of-positive-integer-and-negative-integer.cpp
+++ 2529.maximum-count-of-positive-integer-and-negative-integer/2529.maximum-count-of-positive-integer-and-negative-integer.cpp
@@ -1,18 +1,18 @@
-
-        while (lo < hi) {
-        int lo = 0, hi = n ;
-        // return max(neg,pos);
-         // First positive index
-        //     else if(nums[i]>0) pos++;
-        // }
-        // for(int i=0;i<n;i++){
-        //     if(nums[i]<0) neg++;
-        if((n==2 && nums[0]==0) && nums[1]==0 ) return 0;
-        // int neg=0;
-        // int pos=0;
-            int mid = lo + (hi - lo) / 2;
-
-            if (nums[mid] <= 0)
                 lo = mid + 1;
             else
+                hi = mid;
+        }
+
+        int FPI = lo;
+        int pos = n - FPI;
+
+
+        // First non-negative index
+        lo = 0;
+        hi = n ;
+
+        while (lo < hi) {
+            int mid = lo + (hi - lo) / 2;
+
+            if (nums[mid] < 0)
 
