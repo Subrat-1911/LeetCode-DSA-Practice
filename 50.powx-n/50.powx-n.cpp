4dd14c527f5f2aec6bbc9b21edbc1e36b1d62a73
Subrat Sahoo <subratsahoo28032006@gmail.com>
2026-08-21 09:49:14 +0530

    Auto-sync: Code for 50. Pow(x, n)

--- 50.powx-n/50.powx-n.cpp
+++ 50.powx-n/50.powx-n.cpp
@@ -1,15 +1,15 @@
         while(n1>1){
             x*=x1;
             n1--;
-        }
         else n1=n;
-            n1=-n;
-        if(n<0){
+            n1=-(long long)n;
+        }
+        long long n1=0;
         double x1=x;
-        int n1=0;
+        if(n<0){
     double myPow(double x, int n) {
-public:
-class Solution {
         if(n==0 || x==1) return 1;
         
+class Solution {
+public:
 
