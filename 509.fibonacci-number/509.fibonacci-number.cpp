9a155432afa8a1169da64587873d0e964c563c61
Subrat Sahoo <subratsahoo28032006@gmail.com>
2026-08-24 19:55:39 +0530

    Auto-sync: Code for 509. Fibonacci Number

--- 509.fibonacci-number/509.fibonacci-number.cpp
+++ 509.fibonacci-number/509.fibonacci-number.cpp
@@ -1,9 +1,14 @@
 class Solution {
 public:
-    int fib(int n) {
-        if(n==0) return 0;
+    int fibo(int n, vector<int>& dp){
+        if(n<=1) return n;
+        if(dp[n]!=-1) return dp[n];
+        dp[n]=fibo(n-1,dp)+fibo(n-2,dp);
+        return dp[n];
     }
-        if(n==1) return 1;
-        return fib(n-1)+fib(n-2);
+    int fib(int n) {
+        vector<int> dp(n+1,-1);
+        return fibo(n,dp);
+    }
 };
 
