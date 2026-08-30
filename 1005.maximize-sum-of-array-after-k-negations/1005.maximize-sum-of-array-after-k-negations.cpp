c2ad3cdc1247047294f7389b5721cf3f5eb8b171
Subrat Sahoo <subratsahoo28032006@gmail.com>
2026-08-30 15:51:26 +0530

    Auto-sync: Code for 1005. Maximize Sum Of Array After K Negations

--- 1005.maximize-sum-of-array-after-k-negations/1005.maximize-sum-of-array-after-k-negations.cpp
+++ 1005.maximize-sum-of-array-after-k-negations/1005.maximize-sum-of-array-after-k-negations.cpp
@@ -1,17 +1,17 @@
-            }
-                }
-                    mindx=i;
-            if(min==0) break;
-            else nums[mindx]=-nums[mindx];
-            k--;
-                    min=nums[i];
-            for(int i=0;i<n;i++){
-                if(nums[i]<min){
-        while(k>0){
-        
-        int n=nums.size();
-    int largestSumAfterKNegations(vector<int>& nums, int k) {
+class Solution {
 public:
+    int largestSumAfterKNegations(vector<int>& nums, int k) {
+        int n=nums.size();
+        
+        while(k>0){
             int min=INT_MAX;
             int mindx=-1;
+            for(int i=0;i<n;i++){
+                if(nums[i]<min){
+                    min=nums[i];
+                    mindx=i;
+                }
+            }
+            if(min==0) break;
+            else nums[mindx]=-nums[mindx];
 
