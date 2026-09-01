811cd57c5aa3466ecd55d2e7c18a00a92f2af701
Subrat Sahoo <subratsahoo28032006@gmail.com>
2026-09-02 01:01:59 +0530

    Auto-sync: Code for 204. Count Primes

--- 204.count-primes/204.count-primes.cpp
+++ 204.count-primes/204.count-primes.cpp
@@ -1,6 +1,6 @@
     int countPrimes(int n) {
         int count=0;
-        for(int i=1;i<n+1;i+=2){
+        for(int i=1;i<n;i+=2){
             int x=checkPrime(i);
     }
         }
@@ -8,11 +8,10 @@
             if(x==1) count ++;
         }
         return count;
-    }
             if(n%i==0) return false;
         for(int i=3;i*i<=n;i+=2){
         if(n%2==0) return false;
-        if(n==2) return true;
-        if(n>=2) count++;
+        if(n>2) count++;
+    }
 };
 
