71fd0c39bbba40d2c2f562d53d00a2c8c2f956f6
Subrat Sahoo <subratsahoo28032006@gmail.com>
2026-09-04 10:09:09 +0530

    Auto-sync: Code for 2965. Find Missing and Repeated Values

--- 2965.find-missing-and-repeated-values/2965.find-missing-and-repeated-values.cpp
+++ 2965.find-missing-and-repeated-values/2965.find-missing-and-repeated-values.cpp
@@ -1,21 +1,24 @@
+class Solution {
+public:
+    vector<int> findMissingAndRepeatedValues(vector<vector<int>>& grid) {
+        vector<int>ans;
         int n=grid.size();
         unordered_map<int,int>mp;
+        int sum=0;
         for(int i=0;i<n;i++){
             for(int j=0;j<n;j++){
-        }
                 if(mp.find(grid[i][j]) ==mp.end()){  //not present 
-            }
                     sum+=grid[i][j];
-                }
                     mp[grid[i][j]]++;
-        int sum=0;
-
+                }
                 else{
                     ans.push_back(grid[i][j]);
                 }
-    }
+            }
+        }
         int totalSum=(n*n)*((n*n)+1)/2;
         ans.push_back(totalSum-sum);
         return ans;
-};
+
+    }
 
