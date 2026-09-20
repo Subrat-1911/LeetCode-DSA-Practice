f24cd28072875ee226eb70ed02ef23c0146f8987
Subrat Sahoo <subratsahoo28032006@gmail.com>
2026-09-20 22:02:58 +0530

    Auto-sync: Code for 2706. Buy Two Chocolates

--- 2706.buy-two-chocolates/2706.buy-two-chocolates.cpp
+++ 2706.buy-two-chocolates/2706.buy-two-chocolates.cpp
@@ -1,14 +1,11 @@
-        }
-                first_min=prices[i];
-            }
-        for(int i=0;i<n;i++){
-            if(prices[i]<sec_min && prices[i]!=first_min){
-                sec_min=prices[i];
-            }
-        }
-        int x=(first_min+sec_min);
+        int x=(prices[0]+prices[1]);
         if(money-x<0) return money;
         return money-x;
     }
 };
+class Solution {
+public:
+    int buyChoco(vector<int>& prices, int money) {
+        int n=prices.size();
+        sort(prices.begin(),prices.end());
 
