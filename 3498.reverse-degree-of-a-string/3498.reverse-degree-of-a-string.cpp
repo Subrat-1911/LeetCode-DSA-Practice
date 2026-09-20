7dff8c2754cdf7ecd98836de69a422bcfc3cd0fc
Subrat Sahoo <subratsahoo28032006@gmail.com>
2026-09-20 22:30:25 +0530

    Auto-sync: Code for 3498. Reverse Degree of a String

---
+++ 3498.reverse-degree-of-a-string/3498.reverse-degree-of-a-string.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int reverseDegree(string s) {
+        int n=s.size();
+        int sum=0;
+        for(int i=1;i<n+1;i++){
+            sum+=((122-x)+1)*i;
+        }
+    }
+        return sum;
+            int x=(int)s[i-1];
+};
+
