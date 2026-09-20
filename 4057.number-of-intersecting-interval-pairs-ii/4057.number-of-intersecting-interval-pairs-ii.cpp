fbad472cb815a016a252f5492b9fb802ad411621
Subrat Sahoo <subratsahoo28032006@gmail.com>
2026-09-20 20:21:15 +0530

    Auto-sync: Code for 4057. Number of Intersecting Interval Pairs II

--- 4057.number-of-intersecting-interval-pairs-ii/4057.number-of-intersecting-interval-pairs-ii.cpp
+++ 4057.number-of-intersecting-interval-pairs-ii/4057.number-of-intersecting-interval-pairs-ii.cpp
@@ -1,16 +1,18 @@
-class Solution {
-public:
+            int current_end = intervals[i][1];
+            
+            // Look for the first interval after 'i' that starts AFTER current_end
+            // We use a dummy target vector {current_end, 2e9} to compare with intervals[j][0]
+            auto it = upper_bound(intervals.begin() + i + 1, intervals.end(), vector<int>{current_end, (int)2e9});
+            
+        for (int i = 0; i < n; i++) {
+        // 2. Count overlapping intervals using Binary Search
+        
+        long long count = 0;
+        
+        sort(intervals.begin(), intervals.end());
     long long countIntersectingIntervals(vector<vector<int>>& intervals) {
-        int n=intervals.size();
-        int count=0;
-        for(int i=0;i<n-1;i++){
-            for(int j=i+1;j<n;j++){
-                if(intervals[i][1]>=intervals[j][0] && intervals[i][0]<=intervals[j][1] ){
-                    count++;
-                }
-            }
-        }
-        return count;
-    }
-};
+        // 1. Sort intervals by their start time
+public:
+class Solution {
+        int n = intervals.size();
 
