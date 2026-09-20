f7b386caf9ca205764f20ca4aa6fc0e1d6ba18fa
Subrat Sahoo <subratsahoo28032006@gmail.com>
2026-09-20 20:14:44 +0530

    Auto-sync: Code for 4057. Number of Intersecting Interval Pairs II

---
+++ 4057.number-of-intersecting-interval-pairs-ii/4057.number-of-intersecting-interval-pairs-ii.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    long long countIntersectingIntervals(vector<vector<int>>& intervals) {
+        int n=intervals.size();
+        int count=0;
+        for(int i=0;i<n-1;i++){
+            for(int j=i+1;j<n;j++){
+                if(intervals[i][1]>=intervals[j][0] && intervals[i][0]<=intervals[j][1] ){
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+};
+
