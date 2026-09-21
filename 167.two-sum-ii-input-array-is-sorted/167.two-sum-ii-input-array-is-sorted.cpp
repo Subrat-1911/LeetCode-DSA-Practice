be7b560351c9982016fae6a6ec4cec4fb00ebb0a
Subrat Sahoo <subratsahoo28032006@gmail.com>
2026-09-21 10:21:03 +0530

    Auto-sync: Code for 167. Two Sum II - Input Array Is Sorted

--- 167.two-sum-ii-input-array-is-sorted/167.two-sum-ii-input-array-is-sorted.cpp
+++ 167.two-sum-ii-input-array-is-sorted/167.two-sum-ii-input-array-is-sorted.cpp
@@ -1,3 +1,12 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& numbers, int target) {
+        vector<int> ans;
+        int n = numbers.size();
+        int lo=0;
+        int hi=n-1;
+        while(lo<hi){
+            if(numbers[lo]+numbers[hi]==target){
                 ans.push_back(lo+1);
                 ans.push_back(hi+1);
                 return ans;
@@ -6,13 +15,4 @@
                 hi--;
             }
             else lo++;
-            if(numbers[lo]+numbers[hi]==target){
-        while(lo<hi){
-        int hi=n-1;
-        int lo=0;
-        int n = numbers.size();
-        vector<int> ans;
-    vector<int> twoSum(vector<int>& numbers, int target) {
-public:
-class Solution {
 
