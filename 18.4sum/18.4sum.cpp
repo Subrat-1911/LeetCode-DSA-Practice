f5887a01d19a10dc682a7fefb57cf93981c1bb81
Subrat Sahoo <subratsahoo28032006@gmail.com>
2026-09-21 10:45:48 +0530

    Auto-sync: Code for 18. 4Sum

--- 18.4sum/18.4sum.cpp
+++ 18.4sum/18.4sum.cpp
@@ -1,19 +1,18 @@
-                        hi--;
-                        lo++;
-                        ans.push_back(v);
-                        v.push_back(nums[hi]);
-                        v.push_back(nums[lo]);
-                        v.push_back(k);
-                        v.push_back(s);
-                    if(sum==target){
-                    long long sum=(s+k+nums[lo]+nums[hi]);
-                    vector<int>v;
-                while(lo<hi){
-                long long hi=n-1;
-                long long lo =j+1;
-                long long k=nums[j];
-                long long s=nums[i];
-                if(j>i+1 && nums[j]==nums[j-1]) continue;
-            for(int j=i+1; j<n-2;j++){
+class Solution {
+public:
+    vector<vector<int>> fourSum(vector<int>& nums, int target) {
+        vector<vector<int>>ans;
+        sort(nums.begin(),nums.end());
+        int n=nums.size();
+        for(int i=0;i<n-3;i++){
             if(i>0 && nums[i]==nums[i-1]) continue;
+            for(int j=i+1; j<n-2;j++){
+                if(j>i+1 && nums[j]==nums[j-1]) continue;
+                long long s=nums[i];
+                long long k=nums[j];
+                long long lo =j+1;
+                long long hi=n-1;
+                while(lo<hi){
+                    vector<int>v;
+                    long long sum=(s+k+nums[lo]+nums[hi]);
 
