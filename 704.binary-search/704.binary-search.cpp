dea11e9cc3d54f90160332ba8567abc6a4a65cff
Subrat Sahoo <subratsahoo28032006@gmail.com>
2026-09-21 10:53:52 +0530

    Auto-sync: Code for 704. Binary Search

--- 704.binary-search/704.binary-search.cpp
+++ 704.binary-search/704.binary-search.cpp
@@ -1,3 +1,4 @@
+class Solution {
 public:
     int search(vector<int>& nums, int target) {
         int n=nums.size();
@@ -5,11 +6,11 @@ public:
         int hi=n-1;
         while(lo<=hi){
             int mid=lo+(hi-lo)/2;
-        }
-    }
             if(nums[mid]==target) return mid;
             else if(nums[mid]>target) hi=mid-1;
             else lo=mid+1;
+        }
         return -1;
+    }
 };
 
