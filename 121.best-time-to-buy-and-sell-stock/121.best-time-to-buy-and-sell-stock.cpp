5418f26a5ad67b5b8d015c98c9b673a415c20c0f
Subrat Sahoo <subratsahoo28032006@gmail.com>
2026-09-23 10:51:46 +0530

    Auto-sync: Code for 121. Best Time to Buy and Sell Stock

--- 121.best-time-to-buy-and-sell-stock/121.best-time-to-buy-and-sell-stock.cpp
+++ 121.best-time-to-buy-and-sell-stock/121.best-time-to-buy-and-sell-stock.cpp
@@ -1,2 +1,18 @@
-c
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int n=prices.size();
+        int buy=prices[0];
+        int profit=0;
+        for(int i=0;i<n;i++){
+            if(buy>prices[i]){
+        }
+                buy=prices[i];
+            }
+            if(profit<prices[i]-buy){
+                profit=prices[i]-buy;
+            }
+    }
+        return profit;
+};
 
