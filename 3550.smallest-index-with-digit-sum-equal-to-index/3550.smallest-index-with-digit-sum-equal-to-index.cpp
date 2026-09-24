13021ba18e4a8081c7ca9079576240a3555db225
Subrat Sahoo <subratsahoo28032006@gmail.com>
2026-09-24 09:50:47 +0530

    Auto-sync: Code for 3550. Smallest Index With Digit Sum Equal to Index

---
+++ 3550.smallest-index-with-digit-sum-equal-to-index/3550.smallest-index-with-digit-sum-equal-to-index.cpp
@@ -0,0 +1,18 @@
+    int smallestIndex(vector<int>& nums) {
+        int n=nums.size();
+    }
+        
+        return sum;
+        }
+            n/=10;
+            sum+=x;
+            int x=n%10;
+        while(n>0){
+        int sum=0;
+        }
+            return n;
+        if(n<10){
+    int sumOdDigits(int n){
+public:
+class Solution {
+
